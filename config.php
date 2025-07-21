741a17c0d02d6bf71dd1fe47665a8dfcbac83b8d
Kleponaeru <kelvinlie08@gmail.com>
2025-07-21 21:30:43 +0700

    remove weird spacing

--- config.php
+++ config.php
@@ -4,21 +4,4 @@ $site_url   = "http://localhost/cuymanga";    // URL domain utama
 $url_api    = $site_url;                  // API URL (bisa disesuaikan jika terpisah)
 $image_kit  = "https://ik.imagekit.io//"; // Gunakan ImageKit sendiri, tuk bypass img chapter (daftar di https://imagekit.io)
 $image_og   = "$site_url/storage/og.png"; // url Banner website untuk Open Graph (OG)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // cihuyy
